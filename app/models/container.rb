92e48f6bd8cb7a80cf1c18a3511918debdec893c
Andy Reid <andyreid51@gmail.com>
2014-07-06 21:53:16 +1200

    change container validation

--- app/models/container.rb
+++ app/models/container.rb
@@ -5,8 +5,8 @@ class Container < ActiveRecord::Base
 	belongs_to :comment
 
 	# Validation
-	VALID_CONTAINER_REGEX = ((?:[a-zA-Z0-9]{4}|/{4})[a-zA-Z0-9]{7})
-	validates :container_number, presence: true, format: {with: VALID_CONTAINER_REGEX},
+	#VALID_CONTAINER_REGEX = ((?:[a-zA-Z0-9]{4}|/{4})[a-zA-Z0-9]{7})
+	#validates :container_number, presence: true, format: {with: VALID_CONTAINER_REGEX},
 	validates :date, presence: true
 	validates :bay, numericality: {only_integer: true, less_than_or_equal_to: 40}
 	validates :carrier_id, presence: true
